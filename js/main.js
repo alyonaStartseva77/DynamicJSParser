41f946a0eb4284dbe8da559a8c926657b54d92bf
Alyona Startseva <alyona_startseva.tk@mail.ru>
2021-04-19 00:24:29 +0700

    Added to create form, a button to clear page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
 function generatedForm(obj) {
     console.log(obj)
+
+    addForm(obj)
+    addButtonClear()
+
+}
+
+function addForm(obj) {
+    /*Add a tag form*/
+    let form = document.createElement('form')
+    form.classList.add('generated-form', 'col-md-6')
+    sectionParse.append(form)
+
+    /*Add a title form*/
+    let titleForm = document.createElement('h2')
+    titleForm.textContent = obj.name
+    form.append(titleForm)
+}
+
+function  clearPage() {
+    sectionParse.innerHTML = ''
+}
+
+function addButtonClear() {
+    /*Add a footer for a button*/
+    let footer = document.createElement('footer')
+    footer.classList.add('drop-form', 'col-md-12')
+    sectionParse.append(footer)
+
+    /*Add a button to clear*/
+    let buttonClear = document.createElement('button')
+    buttonClear.classList.add('btn', 'btn-primary', 'my-3', 'custom-button')
+    buttonClear.textContent = 'Clear'
+    footer.append(buttonClear)
+
+    buttonClear.addEventListener('click', clearPage)
 }
 
 function parseJson(result) {
@@ -8,12 +43,14 @@ function parseJson(result) {
 }
 
 function readFileAsString() {
-    let fileReader = new FileReader();
-    fileReader.readAsText(this.files[0]);
+    let fileReader = new FileReader()
+    fileReader.readAsText(this.files[0])
     fileReader.onload = function(){
         parseJson(fileReader.result)
     }
 }
 
-document.querySelector('#file-upload').addEventListener('change', readFileAsString)
+const buttonUpload = document.querySelector('#file-upload')
+const sectionParse = document.querySelector('.parse-json')
 
+buttonUpload.addEventListener('change', readFileAsString)
